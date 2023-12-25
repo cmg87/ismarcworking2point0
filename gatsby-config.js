964948534d1d74d38fe8e9b853ac4e08599dd833
cmg87 <chrisgoerler@gmail.com>
2023-12-24 20:36:09 -0500

    updated gastby config to reflect manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,20 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/marcprofile.png",
+        name: "IsMarcWorkikng",
+        short_name: "IMW",
+        description: "I am always working.",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/marcprofile.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     "gatsby-plugin-sharp",
